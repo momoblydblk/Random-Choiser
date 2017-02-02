98ddb97c671e1ceff20071c0b6a3b61ae5179051
momoblydblk <momoblydblk@gmail.com>
2017-02-01 19:19:33 -0800

    Bug Fixed

--- main.rb
+++ main.rb
@@ -1,15 +1,21 @@
+def random(limit)
+    randNo = rand(limit)
+    return randNo
+end
+
 puts "How many choices do you want to have?"
 choiseNum = gets.chomp
 result = Integer(choiseNum) rescue false
-while result != false && result != 0
+while result*1 == 0 do
     if result == false
         puts "Error: Not a number."
     elsif result == 0
         puts "Invalid Number."
     end
     result = Integer(choiseNum) rescue false
-    return result
+    puts result
 end
+choiseNum = choiseNum.to_i
 allChoise = []
 x = 0
 while x < choiseNum
@@ -18,12 +24,8 @@ while x < choiseNum
     choise = gets.chomp
     allChoise.push(choise)
     puts "Added"
+    x += 1
 end
 finalResultNo = random(choiseNum)
 finalResult = allChoise[finalResultNo]
 puts finalResult
-
-def random(limit)
-    randNo = rand(limit)
-    return randNo
-end
