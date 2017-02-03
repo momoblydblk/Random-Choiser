3a26f1b9c8607d61fadb4a6dc539cd241d930c70
momoblydblk <momoblydblk@gmail.com>
2017-02-02 18:46:53 -0800

    Delete main.rb

--- main.rb
+++
@@ -1,6 +0,0 @@
-puts "Welcome. What do you want to do?\nHere's something you want to do, type 'String', 'Bool' or 'Number' to continue."
-choise = gets.chomp
-case choise
-when String
-  require 'Choise_String.rb'
- when
